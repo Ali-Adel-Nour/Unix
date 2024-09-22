2102508920618b54eb14238d0926a2f1187b4d57
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-09-22 14:31:27 +0300

    testing std

--- playground.js
+++ playground.js
@@ -1,15 +1,30 @@
 const {spawn, exec} = require ("node:child_process")
 
-
-console.log(process.env.PATH)
-
-const subprocess = spawn('bash', ["script.sh"])
+const {stdin, stdout, stderr} = require("node:process")
 
 
+stdin.on("data", (data) => {
 
-subprocess.stdout.on("data",(data)=>{
-  console.log(data.toString("utf-8"))
+  console.log("Got this data from standard in :", data.toString("utf8"));
+
 })
+
+stdout.write("This is some text that i want..")
+stderr.write("This is some text that i may not want")
+
+
+// console.log(process.env.PATH)
+// console.log(process.arg)
+// console.log(process.pid)
+// console.log(process.ppid)
+
+// const subprocess = spawn('bash', ["script.sh"])
+
+
+
+// subprocess.stdout.on("data",(data)=>{
+//   console.log(data.toString("utf-8"))
+// })
 
 // exec("ls -l",(error, stdout, stderr)=>{
 
