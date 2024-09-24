362f11d577033f114e87ad4bee0b3de5cb53c349
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-09-24 11:19:26 +0300

    spawn in node and c

--- playground.js
+++ playground.js
@@ -1,38 +1,62 @@
-const {spawn, exec} = require ("node:child_process")
+const { spawn, exec } = require("node:child_process");
+const { stdin, stdout, stderr, env, argv, pid, ppid } = require("node:process");
 
-const {stdin, stdout, stderr} = require("node:process")
+// ** Standard In, Standard Out, Standard Error */
+// stdin.on("data", (data) => {
+//   // The next 2 lines will do the same thing:
+//   // console.log("Got this data from standard in: ", data.toString("utf8"));
+//   stdout.write(`Got this data from standard in: ${data.toString("utf8")}\n`);
+// });
+// stdout.write("This is some text that I want.\n");
+// stderr.write("This is some text that I may not want.");
 
+// ** Arguments */
+// console.log(argv);
 
-stdin.on("data", (data) => {
+//** Process and Parent Process ID */
+// console.log(pid);
+// console.log(ppid);
 
-  console.log("Got this data from standard in :", data.toString("utf8"));
+//** Environment Variables */
+// console.log(env);
+// console.log(env.MODE);
+// console.log(env.PATH);
+// console.log(env.PWD);
 
-})
+//** Spawning a process */
+const subprocess = spawn(
+  "./playground.js",
+  ["some string", "-f", 34, "some more string", "-u"],
+  {
+    env: { MODE: "development" },
+  }
+);
 
-stdout.write("This is some text that i want..")
-stderr.write("This is some text that i may not want")
+subprocess.stdout.on("data", (data) => {
+  console.log(data.toString("utf-8"));
+});
 
+subprocess.stderr.on("data", (data) => {
+  console.log("Got this stderr from the C app: ", data.toString("utf8"));
+});
 
-// console.log(process.env.PATH)
-// console.log(process.arg)
-// console.log(process.pid)
-// console.log(process.ppid)
+subprocess.stdin.write("Some text that is coming from Node!");
+subprocess.stdin.end();
 
-// const subprocess = spawn('bash', ["script.sh"])
+//** Running a script in a shell */
+// exec(
+//   " source .bashrc && echo 'something string' | tr ' ' '\n'",
+//   {
+//     shell: "/bin/bash",
+//   },
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(error);
+//       return;
+//     }
 
+//     console.log(stdout);
 
-
-// subprocess.stdout.on("data",(data)=>{
-//   console.log(data.toString("utf-8"))
-// })
-
-// exec("ls -l",(error, stdout, stderr)=>{
-
-//   if (error){
-//     console.error(error)
-//     return;
+//     console.log(`stderr: ${stderr}`);
 //   }
-
-//   console.log(stdout)
-//   console.log(`stdeer: ${stderr}`)
-// })
+// );
