c5e4609fef0c71d28ad5e5a1704b75cf0604b1d0
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-10-12 22:39:28 +0300

    adding more workers

--- clustering/cluster.js
+++ clustering/cluster.js
@@ -2,13 +2,33 @@ const cluster = require('node:cluster');
 const coresCount = require('node:os').availableParallelism();
 
 if (cluster.isPrimary) {
+    let requestCount = 0
+    setInterval(() => {
+        console.log(`Total number of requests: ${requestCount}`);
+    }, 5000);
+
+
     for (let i = 0; i < coresCount; i++) {
         const worker = cluster.fork();
+        worker.send("some data")
 
         console.log(
             `This parent spawned a new chlid process with PID ${worker.process.pid}`
         );
     }
+    cluster.on("message",(worker,message) =>{
+        if(message.action && message.action === 'request' ){
+            requestCount++
+        }
+    })
+
+    cluster.on("fork",(worker)=>{
+
+    })
+
+    cluster.on('listening',(worker,adress)=>{
+
+    })
     cluster.on('exit', (worker, code, signal) => {
         console.log(`Worker ${worker.process.id} died. Restarting`);
         cluster.fork();
--- clustering/server.js
+++ clustering/server.js
@@ -3,12 +3,23 @@ const cpeak = require("cpeak")
 const server = new cpeak()
 
 
+
+process.on("message",(message)=>{
+  console.log(`Worker ${process.pid} received message from parent ${message}`)
+})
+
+//process.send("sd")
+
 server.route("get","/",(req,res)=>{
+  process.send({action: "request"})
   res.json({message:"This is some text"})
 })
 
 
 server.route("get","/heavy",(req,res)=>{
+
+  process.send({action: "request"})
+
   for(let i = 0 ; i<10000000000;i++){
 
   }
